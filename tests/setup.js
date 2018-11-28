8ab9a5e97da67edb68f731059935107e471b932f
Chrisantos <ezechris30@gmail.com>
2018-11-28 13:42:39 +0100

    new test commit

--- tests/setup.js
+++ tests/setup.js
@@ -6,3 +6,11 @@ const keys = require('../config/keys');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, {useMongoClient: true});
+
+
+//We create this because jest does not globally require files that are not test files.
+//So we need to require it specially for our test
+
+// In the last section we spoke about how code is not automatically shared between all of our test files
+
+// inside the test directory and anything else in sight inside of our actual project.
